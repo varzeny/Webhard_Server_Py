8fbeac568a1f51800076c7cbca1af5d29f3c0ecb
varzeny <varzeny@gmail.com>
2024-10-29 21:43:40 +0900

    update

--- app/service/ftp.py
+++ app/service/ftp.py
@@ -19,7 +19,7 @@ class Manager:
     @classmethod
     def status(cls):
         result = subprocess.run(
-            args=["sudo", "-S", "systemctl", "is-active", "vsftpd"],
+            ["sudo", "-S", "systemctl", "is-active", "vsftpd"],
             input=cls.env.get("sudopw")+'\n',
             capture_output=True,
             text=True
